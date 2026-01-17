a7b3e0e86dc09929a85d681d03651e14261c744f
Eric Hocking <ehock7229@outlook.com>
2026-01-16 22:11:07 -0500

    fix patch sql

--- sql/postgres/patches/patch-dumps_status.sql
+++ sql/postgres/patches/patch-dumps_status.sql
@@ -1,3 +1,3 @@
 -- Adds status column
 
-ALTER TABLE ata_dump ADD COLUMN dumps_status VARCHAR(100) DEFAULT '' NOT NULL;
+ALTER TABLE data_dump ADD COLUMN dumps_status VARCHAR(100) DEFAULT '' NOT NULL;
